412401d63fd38dec189e0dbccd20572e71233f02
patkrdj <taejun154@gmail.com>
2025-10-25 20:16:33 +0900

    refactor(Application.java): 핵심 기능을 controller로 분리
    Application.java: 메인 로직을 삭제하고 controller 객체를 생성하고 run을 실행시키만 하였음.
    RacingController.java: MCV 패턴을 적용시키 위해 RacingController.java를 생성하고 메인 로직을 옮김.

---
+++ src/main/java/racingcar/Controller/RacingController.java
@@ -0,0 +1,60 @@
+package racingcar.Controller;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingController {
+    public void run() {
+        // 경주할 자동차의 이름 받기
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String racingCarsString = Console.readLine().trim();
+        List<String> racingCars = List.of(racingCarsString.split(","));
+        for (String racingCar : racingCars) {
+            if (racingCar.length() > 5)
+                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
+
+        // 시도 횟수 입력받기
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int tryCount;
+        try {
+            tryCount = Integer.parseInt(Console.readLine().trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수 값을 입력해야 합니다.");
+        }
+
+        // 레이싱 차수 별 실행
+        List<Integer> movingDistances = new ArrayList<>(racingCars.size());
+        for (int i = 0; i < racingCars.size(); i++) movingDistances.add(0);
+        System.out.println("\n실행 결과");
+
+        for (int i = 0; i < tryCount; i++) {
+            for (int j = 0; j < racingCars.size(); j++) {
+                if (Randoms.pickNumberInRange(0, 9) >= 4)
+                    movingDistances.set(j, movingDistances.get(j) + 1);
+            }
+
+            for (int j = 0; j < racingCars.size(); j++) {
+                System.out.println(racingCars.get(j) + " : " + "-".repeat(movingDistances.get(j)));
+            }
+            System.out.println();
+        }
+
+        // 우승자 출력하기
+        List<String> winners = new ArrayList<>();
+        int winnerDistance = 0;
+        for (int i = 0; i < movingDistances.size(); i++) {
+            if (movingDistances.get(i) > winnerDistance) {
+                winnerDistance = movingDistances.get(i);
+                winners.clear();
+                winners.add(racingCars.get(i));
+            } else if (movingDistances.get(i) == winnerDistance) {
+                winners.add(racingCars.get(i));
+            }
+        }
+        System.out.println("최종 우승자: " + String.join(", ", winners));
+    }
+}
