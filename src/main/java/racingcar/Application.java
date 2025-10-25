ed791f6cd9adcb694ae0018994ed70d0bee1de89
patkrdj <taejun154@gmail.com>
2025-10-25 15:24:44 +0900

    feat(Application.java): 경주할 자동차 이름 받기 구현
    java.util.List를 통해 racingCars를 저장하게 하였으며 racingCar가 5자를 초과하면 IllegalArgumenException을 발생하게 하였음.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 경주할 자동차의 이름 받기
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String racingCarsString = Console.readLine().trim();
+        List<String> racingCars = List.of(racingCarsString.split(","));
+        for (String racingCar : racingCars) {
+            if (racingCar.length() > 5)
+                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
+
     }
 }
