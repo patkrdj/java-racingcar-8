1fefffa4af69e4e75d4f7e6c7855482b89b37b92
patkrdj <taejun154@gmail.com>
2025-10-25 21:06:41 +0900

    refactor(RacingController.java): 입출력을 View 폴더 안에서 따로 구현
    inputView.java: racing car의 이름과 시행 횟수을 입력받을 수 있게 하였음.
    outputView.java: 라운드 결과와 우승자를 출력할 수 있게 하였음.

---
+++ src/main/java/racingcar/View/InputView.java
@@ -0,0 +1,30 @@
+package racingcar.View;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class InputView {
+
+    public List<String> readCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String racingCarsString = Console.readLine().trim();
+        List<String> racingCars = List.of(racingCarsString.split(","));
+        for (String racingCar : racingCars) {
+            if (racingCar.length() > 5)
+                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
+        return racingCars;
+    }
+
+    public int readTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int tryCount;
+        try {
+            tryCount = Integer.parseInt(Console.readLine().trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수 값을 입력해야 합니다.");
+        }
+        return tryCount;
+    }
+}
---
+++ src/main/java/racingcar/View/OutputView.java
@@ -0,0 +1,20 @@
+package racingcar.View;
+
+import racingcar.Model.RacingCar;
+
+import java.util.List;
+
+public class OutputView {
+
+    public void printRoundResult(List<RacingCar> racingCars) {
+        System.out.println("\n실행 결과");
+        for (RacingCar racingCar : racingCars) {
+            System.out.println(racingCar.getCarName() + " : " + "_".repeat(racingCar.getMovingDistance()));
+        }
+        System.out.println();
+    }
+
+    public void printWinners(List<String> winners) {
+        System.out.println("최종 우승자: " + String.join(", ", winners));
+    }
+}
