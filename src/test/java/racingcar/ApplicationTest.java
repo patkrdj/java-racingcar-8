0f9cc0530322ef70c35f0e3fbf032b19722df43b
patkrdj <taejun154@gmail.com>
2025-10-27 15:26:30 +0900

    test(ApplicationTest.java): 예외 케이스 추가
    시도횟수에서 정수 값이 아닌 값이 들어온 경우와 음수 값이 들어온 경우를 테스트케이스에 추가하였음.

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -31,6 +31,22 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 시도횟수_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,javaji", "pd"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 시도횟수_음수_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,javaji", "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
